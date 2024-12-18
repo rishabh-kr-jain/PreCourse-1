592d418bcdc83ec1014a35d25787202a39c9e947
Rishabh Kumar Jain <rishabh803@gmail.com>
2024-12-18 14:12:17 -0800

    Precourse 1 Excercise 1 - Done

--- Exercise_1.py
+++ Exercise_1.py
@@ -2,19 +2,30 @@ class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
      def __init__(self):
+          self.arstack = []
          
      def isEmpty(self):
+          if len(self.arstack) == 0:
+               print('stack is empty')
+               return True
+          return False
          
      def push(self, item):
+          self.arstack.append(item)
          
      def pop(self):
+          if not self.isEmpty():
+               self.arstack.pop()
         
         
      def peek(self):
+          print('stack peek method',self.arstack[len[self.arstack]])
         
      def size(self):
+          print('stack size method',len(self.arstack))
          
      def show(self):
+          print('stack show method',self.arstack)
          
 
 s = myStack()
